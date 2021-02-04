52f3a6beeff8cde3d16e6c047b564e2ec34ebb26
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2021-02-04 18:42:36 +0200

    add svg

--- src/Components/AuthNav/AuthNav.js
+++ src/Components/AuthNav/AuthNav.js
@@ -1,25 +1,31 @@
 import { NavLink } from "react-router-dom";
+import { FiUserX } from "react-icons/fi";
 import s from "./authNav.module.css";
 
 export default function AuthNav() {
   return (
-    <nav className={s.container}>
-      <NavLink
-        to="/register"
-        exact
-        className={s.link}
-        activeClassName={s.activeLink}
-      >
-        Реєстрація
-      </NavLink>
-      <NavLink
-        to="/login"
-        exact
-        className={s.link}
-        activeClassName={s.activeLink}
-      >
-        Логін
-      </NavLink>
-    </nav>
+    <div className={s.container}>
+      <span>
+        <FiUserX tyle={{ display: "flex" }} />
+      </span>
+      <nav className={s.container}>
+        <NavLink
+          to="/register"
+          exact
+          className={s.link}
+          activeClassName={s.activeLink}
+        >
+          Реєстрація
+        </NavLink>
+        <NavLink
+          to="/login"
+          exact
+          className={s.link}
+          activeClassName={s.activeLink}
+        >
+          Логін
+        </NavLink>
+      </nav>
+    </div>
   );
 }
--- src/Components/UserMenu/UserMenu.js
+++ src/Components/UserMenu/UserMenu.js
@@ -2,13 +2,17 @@ import { useSelector, useDispatch } from "react-redux";
 import { getUserEmail } from "../../Redux/UserAuth/auth-selectors";
 import { logOut } from "../../Redux/UserAuth/auth-operations";
 import s from "./userMenu.module.css";
+import { FiUserCheck } from "react-icons/fi";
 import { FaSignOutAlt } from "react-icons/fa";
 
 export default function UserMenu() {
   const userEmail = useSelector(getUserEmail);
   const dispatch = useDispatch();
   return (
-    <div>
+    <div className={s.container}>
+      <span>
+        <FiUserCheck tyle={{ display: "flex" }} />
+      </span>
       <span className={s.span}>{userEmail}</span>
       <button
         className={s.button}
